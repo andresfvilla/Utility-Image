7a1a6e887c0c6803a397de58edf0bc83fc223daf
andres.villa@ibm.com <andres.villa@ibm.com>
2017-12-21 16:06:16 -0500

    gpu files

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -26,21 +26,31 @@ done
 date=$(date +%s)
 buildVersion="$date"
 imageName=${dockerRepo}${imagePath}
+
+#Build regular image
 docker build -t "${imageName}:${buildVersion}" -t "${imageName}:latest" .
 buildResult=$?
-if [ $buildResult -eq 0 ]; then
-    echo "Docker build succeeded"
-else
+printf "\n\n\n\n==============FINISHED REGULAR BUILD==============\n\n\n\n"
+
+#Build gpu image
+docker build -t "${imageName}-gpu:${buildVersion}" -t "${imageName}-gpu:latest" ./gpu
+gpuBuildResult=$?
+printf "\n\n\n\n==============FINISHED GPU BUILD==============\n\n\n\n"
+
+
+if [[ ($buildResult -ne 0 || $gpuBuildResult -ne 0)]]; then
   echo "Docker build failed"
   exit 1
 fi
 
 echo "image: ${imageName}:${buildVersion}"
+echo "image: ${imageName}-gpu:${buildVersion}"
 
 while true; do
-    read -p "Do you want to push this version?:" yn
+    read -p "Do you want to push these versions?:" yn
     case $yn in
         [Yy]* ) docker push "${imageName}:${buildVersion}"
+                docker push "${imageName}-gpu:${buildVersion}"
                 exit;;
         [Nn]* ) break;;
         * ) echo "Please answer y or n.";;
